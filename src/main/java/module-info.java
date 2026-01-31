32767d661225392a7c8122a69d746fcc518c69a6
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-01-31 11:47:34 +0100

    direct currency instantiation (#13)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
 module com.pivovarit.typesafe.fx {
     exports com.pivovarit.typesafe.fx;
     exports com.pivovarit.typesafe.fx.currency;
