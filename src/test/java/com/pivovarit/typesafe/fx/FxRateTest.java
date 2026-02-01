428868c53246441a68bb98d64e789581ed2d79c7
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-02-01 10:02:21 +0100

    adjust exchangeUnsafe (#35)

--- src/test/java/com/pivovarit/typesafe/fx/FxRateTest.java
+++ src/test/java/com/pivovarit/typesafe/fx/FxRateTest.java
@@ -9,6 +9,7 @@ import com.pivovarit.typesafe.fx.rate.FxForwardRate;
 import com.pivovarit.typesafe.fx.rate.FxRate;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Currency;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +25,48 @@ class FxRateTest {
         assertThat(inverted.from().equals(rate.to())).isTrue();
         assertThat(inverted.to().equals(rate.from())).isTrue();
         assertThat(inverted.rate().compareTo(new BigDecimal("0.25"))).isZero();
+    }
+
+    @Test
+    void shouldConvert() {
+        Money<USD> money = Money.from("1000", TypedCurrency.USD);
+        FxRate<USD, PLN> rate = FxRate.from("4.00", TypedCurrency.USD, TypedCurrency.PLN);
+
+        Money<PLN> converted = rate.exchange(money);
+        assertThat(converted.amount()).isEqualTo(new BigDecimal("4000.00"));
+        assertThat(converted.currency().currency()).isEqualTo(Currency.getInstance("PLN"));
+    }
+
+    @Test
+    void shouldConvertUnsafeTypedCurrency() {
+        Money<TypedCurrency> money = Money.from("1000", TypedCurrency.from("USD"));
+        FxRate<USD, PLN> usdPlnRate = FxRate.from("4.00", TypedCurrency.USD, TypedCurrency.PLN);
+
+        Money<PLN> converted = usdPlnRate.exchangeUnsafe(money);
+        assertThat(converted.amount()).isEqualTo(new BigDecimal("4000.00"));
+        assertThat(converted.currency().currency()).isEqualTo(Currency.getInstance("PLN"));
+    }
+
+    @Test
+    void shouldConvertUnsafeWildcard() {
+        Money<?> money = Money.from("1000", TypedCurrency.from("USD"));
+        FxRate<USD, PLN> usdPlnRate = FxRate.from("4.00", TypedCurrency.USD, TypedCurrency.PLN);
+
+        Money<PLN> converted = usdPlnRate.exchangeUnsafe(money);
+        assertThat(converted.amount()).isEqualTo(new BigDecimal("4000.00"));
+        assertThat(converted.currency().currency()).isEqualTo(Currency.getInstance("PLN"));
+    }
+
+    @Test
+    void shouldFailConvertUnsafe() {
+        Money<TypedCurrency> money = Money.from("1000", TypedCurrency.from("EUR"));
+        FxRate<USD, PLN> usdPlnRate = FxRate.from("4.00", TypedCurrency.USD, TypedCurrency.PLN);
+
+        assertThatThrownBy(() -> {
+            Money<PLN> converted = usdPlnRate.exchangeUnsafe(money);
+        })
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessageContaining("Money currency EUR does not match rate.from USD");
     }
 
     @Test
